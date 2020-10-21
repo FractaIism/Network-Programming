bfd6b20f3c7ee244cf34d1be39b56be4e09e3389
FractaIism <fractalism.cs05@nctu.edu.tw>
2020-10-22 00:24:17 +0800

    Restructure repository

--- test.py
+++
@@ -1,8 +0,0 @@
-import socket, json, sys
-
-try:
-    csockUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    csockUDP.connect(('localhost', 7854))
-    print(csockUDP)
-except Exception as e:
-    print("Exception: ", e)
